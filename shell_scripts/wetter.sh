da05f9ab63ae568ae3317708696c5b925e4d9485
potzenhotz <lukasmuessle@gmail.com>
2014-01-27 16:47:36 +0100

    final version wetter.sh

--- shell_scripts/wetter.sh
+++ shell_scripts/wetter.sh
@@ -3,11 +3,11 @@
 #curl --silent "http://xml.weather.yahoo.com/forecastrss?p=GMXX0049&u=c" |grep location \
 #|awk -F\" '{print $2":" ;}'
 echo
-echo -e "\033[1mHamburg:\033[0m"
+echo -e "\033[34mHamburg:\033[0m"
 echo
 
 #Local Weather
-echo -e "\033[1mCondition\033[0m"
+echo -e "\033[34mCondition\033[0m"
 
 curl --silent "http://xml.weather.yahoo.com/forecastrss?p=GMXX0049&u=c" |grep condition \
 | awk -F\" '{print "Date: " $8 ;}'
@@ -22,13 +22,13 @@ curl --silent "http://xml.weather.yahoo.com/forecastrss?p=GMXX0049&u=c" |grep wi
 | awk -F\" '{print "Chill: " $2 "\n" "Direction: " $4 "\xc2\xb0" "\n" "Speed: " $6 " km/h";}'
 
 #Sunrise/set
-echo -e "\033[1mDaylight\033[0m"
+echo -e "\033[34mDaylight\033[0m"
 curl --silent "http://xml.weather.yahoo.com/forecastrss?p=GMXX0049&u=c" |grep astronomy \
 | awk -F\" '{print "Sunrise: " $2 "\n" "Sunset: " $4;}'
 
 
 #Forecast
-echo -e "\033[1mForecast\033[0m"
+echo -e "\033[34mForecast\033[0m"
 curl --silent "http://xml.weather.yahoo.com/forecastrss?p=GMXX0049&u=c" | grep High: \
 | sed -e 's/<br \/>//' 
 

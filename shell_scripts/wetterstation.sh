b248c8dd97f111f97d8872ab59d262da01c0a9e1
potzenhotz <lukasmuessle@gmail.com>
2014-06-08 14:48:41 +0200

    wetterstation probe

---
+++ shell_scripts/wetterstation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl  -s --request GET \
+      --header "X-ApiKey: rvAEgT0ijiO1UVUFDBxDuIPoOnnJAelD7CWH1LKOXJuLaTus" \
+      --verbose "https://api.xively.com/v2/feeds/680462944"  
+
+
+echo 'test'
+
